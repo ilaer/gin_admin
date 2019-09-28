12d66bc994930d684d00692961af0ed7caf49508
ilaer <ila2002@gmail.com>
2019-09-28 11:43:29 +0800

    Update init_mysql.go

--- scripts/init_mysql.go
+++ scripts/init_mysql.go
@@ -9,7 +9,7 @@ import (
 
 
 func InitMysql()(success bool,db *sql.DB){
-	const DB_Driver="root:qsw1@tcp(119.23.63.111:3306)/gin_admin"
+	const DB_Driver="root:root@tcp(0.0.0.0:3310)/gin_admin"
 	var isOpen bool
 	db,err:=sql.Open("mysql",DB_Driver)
 	if err!=nil{
